6e7089c3df3ea57c904b8beeb6bb3c6b8c08c49e
DanielBrandao12 <drb12da@gmail.com>
2022-09-29 15:55:45 -0300

    atualização

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 
+const p1 = document.getElementById('position1')
+const p2 = document.getElementById('position2')
+
+var array = ['x']
+
+p1.addEventListener('click',e =>{
+
+    for(let i = 0; i<array.length;i++){
+        if(array[i] == 'x'){
+            p1.innerText = 'O'
+            array.push('o')
+            return
+       }else{
+           p1.innerText = 'X'
+           array.push('x')
+           return
+       }
+
+    }
+
+  
+
+})
+
+p2.addEventListener('click',e =>{
+
+    for(let i = 0; i<array.length;i++){
+        if(array[i] == 'x'){
+            p2.innerText = 'O'
+       }else{
+           p2.innerText = 'X'
+       }
+
+    }
+
+  
+
+})
